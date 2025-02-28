75bcfd48897ece06b70a8f9ff31f32ad61ea69e9
Keita Mitsuhashi <keita.mitsuhashi.83@gmail.com>
2025-02-28 15:01:12 +0900

    Update chat with Groq

--- src/python/chat.py
+++ src/python/chat.py
@@ -1,14 +1,23 @@
-from langchain_core.messages import HumanMessage
-from langchain_openai import ChatOpenAI
+from langchain_core.messages import HumanMessage, AIMessage
+from langchain_groq import ChatGroq
 from morph_lib.stream import stream_chat
 
 import morph
 from morph import MorphGlobalContext
+import os
 
+history = {}
 
 @morph.func
 def langchain_chat(context: MorphGlobalContext):
-    llm = ChatOpenAI(model="gpt-4o")
-    messages = [HumanMessage(context.vars["prompt"])]
-    for token in llm.stream(messages):
+    groq_api_key = os.environ["GROQ_API_KEY"]
+    llm = ChatGroq(model="deepseek-r1-distill-qwen-32b", api_key=groq_api_key)
+
+    if history.get("thread_id") != context.vars["thread_id"]:
+        history["thread_id"] = context.vars["thread_id"]
+        history["messages"] = []
+    history["messages"].append(HumanMessage(content=context.vars["prompt"]))
+    result = ""
+    for token in llm.stream(history["messages"]):
         yield stream_chat(token.content)
+    history["messages"].append(AIMessage(content=result))
